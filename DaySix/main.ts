0e00850505061993068fb0d4d4b5fc09ffb5efd0
Luniem <nico.knuenz@aon.at>
2024-12-09 09:50:33 +0100

    feat: increase exec. speed but  wrong though

--- DaySix/main.ts
+++ DaySix/main.ts
@@ -28,74 +28,72 @@ let currentDirection = 0;
 let isOutOfBounds = false;
 
 const visitedPlaces: Set<string> = new Set();
+const visitedPlacesWithLocation: Set<string> = new Set();
+const bolders: Set<string> = new Set();
+
+const startDate = new Date();
 
 // add start location
 visitedPlaces.add(getLocationKey(startPos));
+let secondSolution = 0;
 let currPosition = startPos;
 
-while (!isOutOfBounds) {
-    while (obstacles.some((obstacle) => obstacle[0] === currPosition[0] + directions[currentDirection][0] && obstacle[1] === currPosition[1] + directions[currentDirection][1])) {
-        currentDirection = (currentDirection + 1) % 4;
+function wouldEndUpInLoop(obs: [number, number][], places: Set<string>, startDirection: number, startPosition: [number, number]): boolean {
+    let isOutOfBounds = false;
+
+    while (!isOutOfBounds) {
+        while (obs.some((obstacle) => obstacle[0] === startPosition[0] + directions[startDirection][0] && obstacle[1] === startPosition[1] + directions[startDirection][1])) {
+            startDirection = (startDirection + 1) % 4;
+        }
+
+        startPosition = [startPosition[0] + directions[startDirection][0], startPosition[1] + directions[startDirection][1]];
+
+        isOutOfBounds = startPosition[0] < 0 || startPosition[1] < 0 || startPosition[0] >= lines.length || startPosition[1] >= lines[0].length;
+        if (isOutOfBounds) {
+            return false;
+        }
+
+        if (places.has(getLocationWithDirectionKey(startPosition, startDirection))) {
+            return true;
+        }
+
+        places.add(getLocationWithDirectionKey(startPosition, startDirection));
     }
 
-    currPosition = [currPosition[0] + directions[currentDirection][0], currPosition[1] + directions[currentDirection][1]];
+    return false;
+}
 
-    isOutOfBounds = currPosition[0] < 0 || currPosition[1] < 0 || currPosition[0] >= lines.length || currPosition[1] >= lines[0].length;
-    if (!isOutOfBounds) {
-        visitedPlaces.add(getLocationKey(currPosition));
+function test(obstacles: [number, number][], currPosition: [number, number], currentDirection: number) {
+    while (!isOutOfBounds) {
+        while (
+            obstacles.some((obstacle) => obstacle[0] === currPosition[0] + directions[currentDirection][0] && obstacle[1] === currPosition[1] + directions[currentDirection][1])
+        ) {
+            currentDirection = (currentDirection + 1) % 4;
+        }
+
+        currPosition = [currPosition[0] + directions[currentDirection][0], currPosition[1] + directions[currentDirection][1]];
+
+        isOutOfBounds = currPosition[0] < 0 || currPosition[1] < 0 || currPosition[0] >= lines.length || currPosition[1] >= lines[0].length;
+        if (!isOutOfBounds) {
+            visitedPlaces.add(getLocationKey(currPosition));
+            visitedPlacesWithLocation.add(getLocationWithDirectionKey(currPosition, currentDirection));
+        }
+
+        // check what happens if place a obstacle in front
+        const newObs: [number, number] = [currPosition[0] + directions[currentDirection][0], currPosition[1] + directions[currentDirection][1]]; // right in front of us
+        if (wouldEndUpInLoop([...obstacles, newObs], new Set(visitedPlacesWithLocation), currentDirection, [...currPosition])) {
+            bolders.add(getLocationKey(newObs));
+        }
     }
 }
+
+test(obstacles, currPosition, currentDirection);
 
 console.log(visitedPlaces.size);
 
-const visitedPlacesWithLocation: Set<string> = new Set();
-
-//brutoforce our way through this ==> just try all position once, when we enter a same position with the same location, we stuck in a loop
-let secondSolution = 0;
-
-for (let y = 0; y < lines.length; y++) {
-    console.log(y);
-    for (let x = 0; x < lines[0].length; x++) {
-        let currentPosition = startPos;
-        const newObstacles: [number, number][] = [...obstacles, [y, x]];
-        currentDirection = 0;
-        isOutOfBounds = false;
-        visitedPlacesWithLocation.clear();
-        visitedPlacesWithLocation.add(getLocationKey(currentPosition));
-
-        if (y == 6 && x == 6) {
-            debugger;
-        }
-
-        while (!isOutOfBounds) {
-            while (
-                newObstacles.some(
-                    (obstacle) => obstacle[0] === currentPosition[0] + directions[currentDirection][0] && obstacle[1] === currentPosition[1] + directions[currentDirection][1]
-                )
-            ) {
-                currentDirection = (currentDirection + 1) % 4;
-            }
-
-            currentPosition = [currentPosition[0] + directions[currentDirection][0], currentPosition[1] + directions[currentDirection][1]];
-
-            isOutOfBounds = currentPosition[0] < 0 || currentPosition[1] < 0 || currentPosition[0] >= lines.length || currentPosition[1] >= lines[0].length;
-            if (!isOutOfBounds) {
-                const locationKey = getLocationWithDirectionKey(currentPosition, currentDirection);
-                if (visitedPlacesWithLocation.has(locationKey)) {
-                    break;
-                } else {
-                    visitedPlacesWithLocation.add(locationKey);
-                }
-            }
-        }
-
-        if (!isOutOfBounds) {
-            secondSolution++;
-        }
-    }
-}
-
-console.log(secondSolution);
+const endDate = new Date();
+console.log(endDate.getTime() - startDate.getTime());
+console.log('finished', bolders.size);
 
 function getLocationKey(location: [number, number]): string {
     return `${location[0]}-${location[1]}`;
