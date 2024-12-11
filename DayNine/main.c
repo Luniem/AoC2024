002a9d92436a8d564425ae7380d2854e12614aa4
Nico KNUENZ <nico.KNUENZ@bachmann.info>
2024-12-11 16:24:09 +0100

    feat: lord have mercy - does not work

--- DayNine/main.c
+++ DayNine/main.c
@@ -6,8 +6,13 @@
 typedef struct {
     uint16_t start;
     uint16_t end;
-    uint8_t id;
+    uint32_t id;
 } FileRange;
+
+int calcSolution(int index, int id) {
+    printf("Index: %d, ID: %d\n", index, id);
+    return index * id;
+}
 
 int main() {
     FILE* file = fopen("./input.txt", "r");
@@ -22,27 +27,79 @@ int main() {
         // read line into buffer
         fgets(line, sizeof(line), file);
 
+        int currentIndex = 0;
         int rangesSize = 0;
         FileRange *ranges = (FileRange *)malloc(rangesSize * sizeof(FileRange));
-
-        int currentIndex = 0;
+        
         for(int i = 0; i < strlen(line); i++) {
-            printf("Index: %d\n", i);
             int length = line[i] - '0';
             if(i % 2 == 0) {
-                // even -- file
-                printf("Even %d\n", rangesSize);
-
+                // even number - it is a file
                 ranges = (FileRange *)realloc(ranges, sizeof(FileRange) * ++rangesSize);
-                printf("after malloc");
-                ranges[currentIndex].start = currentIndex;
-                ranges[currentIndex].end = length - 1;
-                ranges[currentIndex].id = rangesSize - 1;
+
+                ranges[rangesSize - 1].start = currentIndex;
+                ranges[rangesSize - 1].end = currentIndex + length - 1;
+                ranges[rangesSize - 1].id = rangesSize - 1;
             }
+
             currentIndex += length;
         }
 
-        printf("Ranges: %d\n", rangesSize);
+        int lastUsedFreeIndex = ranges[0].end;
+
+        // go from behind 
+        for (int i = rangesSize - 1; i >= 0; i--)
+        {
+            if(lastUsedFreeIndex > ranges[i].end) {
+                // already completely placed
+                // we dont need to move and just need to add
+                for(int j = ranges[i].start; j <= ranges[i].end; j++) {
+                    solution += calcSolution(j, ranges[i].id);
+                }
+            }else {
+                // we can shift it into another place
+                int spaceLeftToPlace = ranges[i].end - ranges[i].start + 1;
+                // check if we still have to place something
+                while(spaceLeftToPlace > 0){
+                    // search how much free space until next file
+                    for(int x = 0; x < rangesSize; x++) {
+
+
+                        if(ranges[x].start > lastUsedFreeIndex) {
+                            if(ranges[x].id != ranges[i].id) {
+
+                                // go until next file
+                                for (int j = lastUsedFreeIndex + 1; j < ranges[x].start && spaceLeftToPlace > 0; j++)
+                                {
+                                    solution += calcSolution(j, ranges[i].id);
+                                    spaceLeftToPlace--;
+                                    lastUsedFreeIndex = j;
+                                }
+
+                                // set last used free index to the end of the file
+                                if(spaceLeftToPlace > 0) {
+                                    lastUsedFreeIndex = ranges[x].end;
+                                }
+                            } else {
+
+                                for(int j = lastUsedFreeIndex + 1; j <= lastUsedFreeIndex + spaceLeftToPlace + 1 && spaceLeftToPlace > 0; j++) {
+                                    solution += calcSolution(j, ranges[i].id);
+                                    spaceLeftToPlace--;
+                                    lastUsedFreeIndex = j;
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+                // remove out of list
+                ranges[i].start = -1;
+                ranges[i].end = -1;
+            }
+        }
+
+    printf("%d\n", solution);
 
         // close file
         fclose(file);
@@ -51,5 +108,5 @@ int main() {
         return 1;
     }
 
-    return solution;
+    return 0;
 }
